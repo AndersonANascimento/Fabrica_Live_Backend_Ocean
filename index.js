1f54f21c138d30fabed6b4410b706ef0bf8b2b03
Anderson Abreu do Nascimento <anderson.abreu@prodam.am.gov.br>
2023-05-15 14:50:48 -0400

    Listagem/criação de enpoints de leitura de mensagem

--- index.js
+++ index.js
@@ -7,7 +7,27 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
-app.post('/hello', (req, res) => {
+/* Lista de endpoints */
+const mensagens = [
+    "Essa é a 1ª mensagem",
+    "Essa é a 2ª mensagem",
+];
+
+app.get('/mensagens', (req, res) => {
+    res.send(mensagens);
+});
+app.get('/mensagens/:id', (req, res) => {
+    const id = req.params.id - 1;
+    const mensagem = mensagens[id];
+    res.send(mensagem);
+});
+app.post('/mensagens', (req, res) => {
+    res.send('Hello!');
+});
+app.put('/mensagens', (req, res) => {
+    res.send('Hello!');
+});
+app.delete('/mensagens', (req, res) => {
     res.send('Hello!');
 });
 
